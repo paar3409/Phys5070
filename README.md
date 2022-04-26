01b2469c033a2b90508097ba545a89495f07a5b2
paar3409 <70716523+paar3409@users.noreply.github.com>
2022-04-25 18:54:07 -0600

    Testing+Hamiltonian
    Created a test to find out how good my transmon Hamiltonian is compared to other known solutions. Added the full Hamiltonian of the system as well

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Final project repository to reproduce results from https://arxiv.org/pdf/2203.11
 Goal is to recreate the figures of merit from the paper utilizing a much smaller computational power. This should give some insight into the relevant system dynamics. The figures of merit that this code reproduces are ...
 
 The core of this work is in the Jupyter notebook. There will be a final version of the code that is designed for cluster usage. The cluster script is designed to run as many levels as possible and produce as close an image as possible to the final paper figures.
+
+This simulation is particularly hard because the dimensions quickly increase out of control. Managing the simulation time is the main challenge, although that might not be totally feasible on a simple code that aims to reproduce results from others.
--- Test_Hamiltonian_vs_scqubits.py
+++ Test_Hamiltonian_vs_scqubits.py
@@ -9,20 +9,42 @@ test is done against results from scqubits, which are known to be correct.
 """
 import numpy as np
 import scqubits
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
+import qutip as qt
 #import testing stuff
-
+import numpy.testing as npt
 
 #Frist up set up the qubit parameters:
 
-EC = 280
-EJ = 50*EC
-ng_list = np.linspace(-1, 1, 220)
+E_C = 280
+E_J = 50*E_C
+ng_list = np.linspace(-1, 1, 250)
+N=100
 
-transmon = scqubits.Transmon(EJ=30.02,
-                              EC=1.2,
+transmon = scqubits.Transmon(EJ=E_J,
+                              EC=E_C,
                               ng=.5,
-                              ncut=31)
-eigenvals = transmon.get_spectrum_vs_paramvals('ng', ng_list, evals_count=5)
+                              ncut=N)
+eigenvals = transmon.get_spectrum_vs_paramvals('ng', ng_list, evals_count=N)
 #eigenvals.energy_table now contains the eigenvalues calculated from scqubits
-transmon.plot_evals_vs_paramvals('ng', ng_list, evals_count=5, subtract_ground=False)
+
+def hamiltonian(Ec, Ej, N, ng):
+    """
+    Return the transmon hamiltonian as a Qobj instance.
+    N is the order that we are using.
+    Ec is the charging energy.
+    Ej is the Josephson energy.
+    ng is the charge of the qubit
+    """
+    m = np.diag(4 * Ec * (np.arange(-N,N+1)-ng)**2) + 0.5 * Ej * (np.diag(-np.ones(2*N), 1) + 
+                                                               np.diag(-np.ones(2*N), -1))
+    return qt.Qobj(m)
+
+energies = np.array([hamiltonian(E_C, E_J, N, ng).eigenenergies(eigvals=N) for ng in ng_list])
+
+
+diffsum = np.sum(np.abs(eigenvals.energy_table-energies))
+npt.assert_allclose(diffsum, 0, atol=1e-9)
+#This means that the error in less than 1Hz over the total sampled spectrum, which is 25,000 samples
+print('Our transmon hamiltonian is within our desired tolerance! ' +
+      'That is less than 1 Hz difference over 25,000 spots')
